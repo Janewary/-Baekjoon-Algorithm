94ecacca109dccf8b80c7bf4235d44ebf6beab17
CindyKong <63396836+KongSoobin@users.noreply.github.com>
2022-05-05 15:51:50 +0900

    [Bronze IV] Title: 사분면 고르기, Time: 124 ms, Memory: 14116 KB -BaekjoonHub

---
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Main{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int x = Integer.parseInt(br.readLine());
+        int y = Integer.parseInt(br.readLine());
+        
+        if(x > 0){
+            if(y > 0) System.out.println("1");
+            if(y < 0) System.out.println("4");
+        } else if(x < 0){
+            if(y > 0) System.out.println("2");
+            if(y < 0) System.out.println("3");
+        }
+    }
+}
