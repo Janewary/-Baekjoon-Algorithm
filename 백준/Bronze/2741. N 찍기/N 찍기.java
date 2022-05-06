1c5d6f0050572d5f870880f37c2610fb8c283b5f
CindyKong <63396836+KongSoobin@users.noreply.github.com>
2022-05-06 22:54:03 +0900

    [Bronze III] Title: N 찍기, Time: 292 ms, Memory: 21092 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+
+public class Main{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        int n = Integer.parseInt(br.readLine());
+        for(int i=1; i<n+1; i++){
+            bw.write(i+"\n");
+        }
+        
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+}
