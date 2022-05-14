fc103649e9dd2b3d52609f80c8234ce888364408
CindyKong <63396836+KongSoobin@users.noreply.github.com>
2022-05-14 21:15:12 +0900

    [Bronze III] Title: 최소, 최대, Time: 504 ms, Memory: 92224 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.java
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -1,22 +1,32 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
-    
+import java.util.StringTokenizer;
+
 public class Main{
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        String[] arr = (br.readLine()).split(" ");
-        int min = Integer.parseInt(arr[0]);
-        int max = Integer.parseInt(arr[0]);
+        int[] str = new int[N];
         
-        for(int i=0; i<arr.length; i++){
-            if(min > Integer.parseInt(arr[i])){
-                min = Integer.parseInt(arr[i]);
-            } else if(max < Integer.parseInt(arr[i])){
-                max = Integer.parseInt(arr[i]);
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        for(int i=0; i<N; i++){
+            str[i] = Integer.parseInt(st.nextToken());
+        }
+        int min = str[0];
+        int max = str[0];
+        
+        for(int i=0; i<N; i++){
+            if(min > str[i]){
+                min = str[i];
+            }
+            if(max < str[i]){
+                max = str[i];
             }
         }
         System.out.println(min + " " + max);
+        
+        
+        
     }
 }
