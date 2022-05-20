7db5005bbaeb7a5b10aaacd9733278fc91672527
CindyKong <63396836+KongSoobin@users.noreply.github.com>
2022-05-20 20:37:34 +0900

    [Bronze II] Title: 나머지, Time: 120 ms, Memory: 14024 KB -BaekjoonHub

--- 백준/Bronze/3052. 나머지/나머지.java
+++ 백준/Bronze/3052. 나머지/나머지.java
@@ -1,17 +1,21 @@
-import java.util.HashSet;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
 
 public class Main{
     public static void main(String[] args) throws IOException {
+        boolean[] arr = new boolean[42];
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        HashSet<Integer> h = new HashSet<Integer>();
-       
-        for(int i=0; i<10; i++){
-            h.add(Integer.parseInt(br.readLine()) % 42);
+        
+        for(int i=0; i<10; i++) {
+            arr[Integer.parseInt(br.readLine()) % 42] = true;
         }
         
-        System.out.println(h.size());
+        int count = 0;
+        for(boolean value : arr) {
+            if(value) count++;
+        }
+        
+         System.out.println(count);
     }
 }
